58658737ab414e805321c57a4fdea92a9f543601
hang <isixline@qq.com>
2021-06-12 12:58:37 +0800

    fix: created_at and updated_at not null default

--- domain/src/main/resources/db/migration/V1__add_user_table.sql
+++ domain/src/main/resources/db/migration/V1__add_user_table.sql
@@ -5,6 +5,6 @@ CREATE TABLE user
     `email`      VARCHAR(50),
     `password`   VARCHAR(100) NOT NULL,
     `role`       VARCHAR(10)  NOT NULL,
-    `created_at` TIMESTAMP(3) NOT NULL,
-    `updated_at` TIMESTAMP(3) NOT NULL
+    `created_at` TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)
 );
