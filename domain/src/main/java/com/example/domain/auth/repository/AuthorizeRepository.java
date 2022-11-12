6c18f5f2fc65a630e02561787c37ece6b17f20da
hang <isixline@qq.com>
2022-11-12 16:21:18 +0800

    refactor: add infrastructure

--- domain/src/main/java/com/example/domain/auth/repository/AuthorizeRepository.java
+++ domain/src/main/java/com/example/domain/auth/repository/AuthorizeRepository.java
@@ -1,37 +1,13 @@
 package com.example.domain.auth.repository;
 
 import com.example.domain.auth.model.Authorize;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.concurrent.TimeUnit;
-
-@Slf4j
 @Repository
-public class AuthorizeRepository {
-    public static final long DEFAULT_EXPIRE = 24 * 60 * 60;
-    @Autowired
-    private RedisTemplate<String, Authorize> redisTemplate;
+public interface AuthorizeRepository {
+    Authorize create(Authorize authorize);
 
-    public Authorize create(Authorize authorize) {
-        authorize.setExpire(DEFAULT_EXPIRE);
-        redisTemplate.boundValueOps(generateKey(authorize.getId()))
-                .set(authorize, DEFAULT_EXPIRE, TimeUnit.SECONDS);
-        return authorize;
-    }
+    Authorize get(String id);
 
-    public Authorize get(String id) {
-        String key = generateKey(id);
-        return redisTemplate.opsForValue().get(key);
-    }
-
-    public void delete(String id) {
-        redisTemplate.delete(generateKey(id));
-    }
-
-    private String generateKey(String id) {
-        return "token:" + id;
-    }
+    void delete(String id);
 }
---
+++ infrastructure/src/main/java/com/example/infrastructure/AuthorizeRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.example.infrastructure;
+
+import com.example.domain.auth.model.Authorize;
+import com.example.domain.auth.repository.AuthorizeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Repository
+public class AuthorizeRepositoryImpl implements AuthorizeRepository {
+    public static final long DEFAULT_EXPIRE = 24 * 60 * 60;
+    @Autowired
+    private RedisTemplate<String, Authorize> redisTemplate;
+
+    @Override
+    public Authorize create(Authorize authorize) {
+        authorize.setExpire(DEFAULT_EXPIRE);
+        redisTemplate.boundValueOps(generateKey(authorize.getId()))
+                .set(authorize, DEFAULT_EXPIRE, TimeUnit.SECONDS);
+        return authorize;
+    }
+
+    @Override
+    public Authorize get(String id) {
+        String key = generateKey(id);
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    @Override
+    public void delete(String id) {
+        redisTemplate.delete(generateKey(id));
+    }
+
+    private String generateKey(String id) {
+        return "token:" + id;
+    }
+}
